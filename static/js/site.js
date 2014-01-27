b4f0c163d193d897e311f033f75f4ba8f7541e81
Zhu Yuzhou <space.fd@gmail.com>
2014-01-27 13:20:40 +1100

    Completed the new website

--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,6 @@
 $(function(){
     show_next_quote();
+    setTimeout('show_next_screenshot()', 3000);
 });
 
 function show_next_quote(){
@@ -30,4 +31,22 @@ function show_next_quote(){
             setTimeout('show_next_quote()', seconds);
         });
     }
+}
+
+function show_next_screenshot(){
+    var number_of_shots = $('.screen-shots li').length;
+    var current_screen = parseInt($('.screen-shots').attr('current-screen'));
+    current_screen ++;
+    if (current_screen == number_of_shots){
+        current_screen = 0;
+        $('.screen-shots').css('left', 0);
+        setTimeout('show_next_screenshot()', 3000);
+         $('.screen-shots').attr('current-screen', current_screen);
+        return;
+    } 
+
+    $('.screen-shots').animate({left:-320*current_screen}, 250, function(){
+        $('.screen-shots').attr('current-screen', current_screen);
+        setTimeout('show_next_screenshot()', 3000);
+    });
 }
