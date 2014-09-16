2b47de163369f752d6b23fbc9317ed821fd68cf1
Zack Zhu <zackz@woolloo.com>
2014-09-16 12:21:34 +1000

    Updated 5coins web page

--- static/js/site.js
+++ static/js/site.js
@@ -1,27 +1,5 @@
 $(function(){
-    var now = new Date();
-    var hours = now.getHours();
-
-    if (hours < 6){
-        $('#bg img').prop('src','static/img/bg/1.jpg');
-    }else if (hours >=6 && hours <= 9){
-        $('#bg img').prop('src','static/img/bg/2.jpg');
-    }else if (hours >9 && hours <=16){
-        $('#bg img').prop('src','static/img/bg/3.jpg');
-        //$('body').addClass('light');
-    }else if (hours >16 && hours <=19){
-        $('#bg img').prop('src','static/img/bg/4.jpg');
-    }else{
-        $('#bg img').prop('src','static/img/bg/5.jpg');
-    }
-
-    // build reviews
-    for (var i in reviews){
-        var li = $('<li><p class="by"><strong>by '+reviews[i].name+'</strong> - '+reviews[i].date+'</p><p>'+reviews[i].content+'</p></li>');
-        $('#reviews').append(li);
-    }
-    show_next_quote();
-    setTimeout('show_next_screenshot()', 4000);
+    
 });
 
 function show_next_quote(){
