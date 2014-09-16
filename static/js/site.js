432aea7f562ba54165c5e5a2522ef8a42918c58d
Zack Zhu <zackz@woolloo.com>
2014-09-16 13:19:35 +1000

    Added layer animation effect

--- static/js/site.js
+++ static/js/site.js
@@ -1,53 +1,31 @@
 $(function(){
-    
+    $('.layer').each(function(){
+        var original_y = parseInt($(this).css('top').replace('px',''));
+        $(this).attr('original-y', original_y);
+        var scroll_offset = parseInt($(this).attr('scroll-offset'));
+        $(this).attr('max-y', original_y - scroll_offset);
+    });
+
+    $(window).scroll(function(){
+        scroll_layer();
+    });
+
+    scroll_layer();
 });
 
-function show_next_quote(){
-    var quote = $('.quotes li:first');
+function scroll_layer(){
+    var scroll_top = $(window).scrollTop();
+    var layer_delta = 0;
 
-    if ($('.quotes li.showing').length > 0){
-        quote = $('.quotes li.showing').next('li');
-        if (quote.length == 0) quote = $('.quotes li:first');
-
-        $('.quotes li.showing').animate({opacity:0}, 1000, function(){
-            $(this).hide();
-            $(this).removeClass('showing');
-            quote.addClass('showing').show().css('opacity','0').animate({opacity:1}, 1000, function(){
-                var seconds = $(this).find('p:last').html().split(' ').length * 200;
-                if (seconds < 3000){
-                    seconds = 3000;
-                }
-
-                setTimeout('show_next_quote()', seconds);
-            });
-        });
-    }else{
-         quote.addClass('showing').show().css('opacity','0').animate({opacity:1}, 1000, function(){
-            var seconds = $(this).find('p:last').html().split(' ').length * 200;
-            if (seconds < 3000){
-                seconds = 3000;
-            }
-            setTimeout('show_next_quote()', seconds);
-        });
-    }
-}
-
-function show_next_screenshot(){
-    var showing_view = $('.screen-shots li.showing');
-    var next_view = showing_view.prev('li');
-
-    if (next_view.length == 0){
-        showing_view.removeClass('showing');
-        next_view = $('.screen-shots li:last');
-        next_view.addClass('showing').animate({opacity:1}, 1000, function(){
-            $('.screen-shots li').css('opacity', '1');
-            setTimeout('show_next_screenshot()', 4000);
-        });
-    }else{
-        showing_view.animate({opacity:0}, 1000, function(){
-            showing_view.removeClass('showing');
-            next_view.addClass('showing');
-            setTimeout('show_next_screenshot()', 4000);
+    if (scroll_top > 500){
+        layer_delta = (scroll_top - 500) / 3;
+        $('.layer').each(function(){
+            var original_y = parseInt($(this).attr('original-y'));
+            var max_y = parseInt($(this).attr('max-y'));
+            var ratio = parseInt($(this).attr('scroll-ratio'));
+            var new_top = original_y - layer_delta * ratio;
+            new_top = Math.max(new_top, max_y);
+            $(this).css('top', new_top+'px');
         });
     }
 }
